b1d5342569f6ca21abcfa2bcb3a31de0ec7d652a
Rianna Castro <riannaxcastro@gmail.com>
2022-12-16 22:24:52 -1000

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rianna_account = Account.create!(
+  account_number: "123456789",
+  account_type: "checking",
+  available_balance: "80000",
+  total_balance: "100000"
+)
+
+User.create!(
+  first_name: "Rianna",
+  last_name: "Castro",
+  username: "riannac",
+  email: "rianna@example.com",
+  account: rianna_account
+)
