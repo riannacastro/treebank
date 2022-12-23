64b404075479d3ae69d52f699ac1785f5a95b395
Rianna Castro <riannaxcastro@gmail.com>
2022-12-22 19:10:03 -1000

    return account from mutation

--- app/graphql/mutations/create_account.rb
+++ app/graphql/mutations/create_account.rb
@@ -2,13 +2,15 @@ module Mutations
   class CreateAccount < BaseMutation
     argument :account_attributes, Types::AccountAttributes, required: true
 
+    field :account, Types::AccountType, null: false
+
     def resolve(account_attributes:)
 
       Account.create!(account_attributes.to_h)
 
-      # if account.valid?
-      #   { account: account }
-      # end
+      if account.valid?
+        { account: account }
+      end
     end
 
   end
